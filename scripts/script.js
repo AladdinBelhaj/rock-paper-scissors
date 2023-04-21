19cd063beb470b9ad9640f82012256fa800d7e6d
AladdinBelhaj <alabelhaj2002@gmail.com>
2023-04-21 03:29:42 +0100

    Add animation to UI

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 const btns = document.querySelectorAll('button');
+let playerScore = 0;
+let computerScore = 0;
 
 btns.forEach(btn =>{
     btn.addEventListener('click',playRound);
@@ -20,22 +22,55 @@ function getComputerChoice(){
  function playRound(){
     const computerChoice = getComputerChoice();
     const playerChoice = this.id;
+    test(playerChoice);
     if(playerChoice == computerChoice){
         console.log(`Draw!`);
     }else if(playerChoice=="rock" && computerChoice=="scissors"){
         console.log(`You win! Rock beats scissors!`);
+        playerScore++;
     }else if(playerChoice=="scissors" && computerChoice=="paper"){
         console.log(`You win! Scissors beat paper!`);
+        playerScore++;
     }else if(playerChoice=="paper" && computerChoice=="rock"){
         console.log(`You win! Paper beats rock!`);
+        playerScore++;
     }else if(computerChoice=="rock" && playerChoice=="scissors"){
         console.log(`You lose! Rock beats scissors!`);
+        computerScore++;
     }else if(computerChoice=="scissors" && playerChoice=="paper"){
         console.log(`You lose! Scissors beat paper!`);
+        computerScore++;
     }else if(computerChoice=="paper" && playerChoice=="rock"){
         console.log(`You lose! Paper beats rock!`);
+        computerScore++;
     }
+
+    if(playerScore == 5){
+        console.log('You win the game!');
+        computerScore = 0;
+        playerScore = 0;
+    }else if(computerScore == 5){
+        console.log('You lose the game!');
+        computerScore = 0;
+        playerScore = 0;
+    }
+
+}
+
+function test(playerChoice){
+    container = document.querySelector('#container');
+    img = document.createElement('img');
+    img.src = `img/${playerChoice}.png`;
+    container.appendChild(img);
+    rotate();
 }
 
 
 
+function rotate(){
+    img.classList.add('rotate-90-br-cw');
+    img.addEventListener('animationend',function(){
+        img.classList.remove('rotate-90-br-cw');
+        img.classList.add('rotate-90-br-cw-normal')
+    },{once:true});
+}
